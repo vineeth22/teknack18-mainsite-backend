469ee8d5b71aa0e9ddbebc01d9b15bc341cc2eff
Vineeth Suvarna <vineeth22@live.in>
2018-02-10 17:22:07 +0530

    Create registration and login routes

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const auth = require('./modules/authentication.js');
 
-const user = {
-  code: 'ASDFK',
-  username: 'testing',
-  email: 'asd@asd.com',
-  password: 'asdfg',
-  passwordConf: 'asdfg'
-};
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+
+/*
+Post data: user.code, user.username, user.email, user.password,
+  user.passwordConf
+Responses: Incomplete data, Passwords do not match, Code used or does not exist,
+  Username exists, Registration successful
+*/
 app.post('/register', (req, res, next) => {
+  const user = req.body.user;
   auth.register(user)
-    .then((result) => {
-      res.send(result);
-    })
-    .catch(next);
+    .then((result) => res.send(result))
+    .catch((err) => next(err));
 });
 
-
+/*
+Post data: user.username, user.password
+Responses: Incomplete data, Login Successful, Username or password incorrect
+*/
 app.post('/login', (req, res, next) => {
+  const user = req.body.user;
   auth.login(user)
-    .then(res.send)
-    .catch(next);
+    .then((result) => res.send(result))
+    .catch((err) => next(err));
 });
 
 app.use((err, req, res, next) => {
-  //console.log(err);
-  //console.log(req);
-  //console.log(res);
-// console.log(err);
- res.sendStatus(500);
+  console.log(err);
+  res.sendStatus(500);
+  next();
 });
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
